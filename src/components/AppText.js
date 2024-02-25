9f2f5cec74bf5af6eb08fa40877b5b034006e824
whoafridi <afridirk10@gmail.com>
2024-02-25 13:08:29 +0600

    create card , apptext component

---
+++ src/components/AppText.js
@@ -0,0 +1,12 @@
+import { Platform, StyleSheet, Text } from "react-native";
+
+export default function AppText({ children, style }) {
+  return <Text style={[styles.text, style]}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Inter",
+  },
+});
