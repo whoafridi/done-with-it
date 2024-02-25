37ccf09df620a57f4711ffa30cf9ef6668648c31
whoafridi <afridirk10@gmail.com>
2024-02-25 12:10:33 +0600

    improve welcome screen, button component

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,19 +1,22 @@
 import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 
-import colors from "../config/colors";
+import AppButton from "../components/AppButton";
 
 export default function WelcomeScreen() {
   return (
     <ImageBackground
+      blurRadius={10}
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
       <View style={styles.logoConatiner}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text>SEll what you don't need</Text>
+        <Text style={styles.tagline}>Sell what you don't need</Text>
       </View>
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.buttonContainer}>
+        <AppButton title="Login" />
+        <AppButton title="Register" color="secondary" />
+      </View>
     </ImageBackground>
   );
 }
@@ -33,14 +36,13 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
   },
-  loginButton: {
+  buttonContainer: {
+    padding: 20,
     width: "100%",
-    height: 70,
-    backgroundColor: colors.primary,
   },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: colors.secondary,
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
   },
 });
